1fd3defc3bbdf31cf9099e80c274a21ca4bc40cd
unknown <sivaranjinikavi97.com>
2024-02-06 09:55:54 +0530

    Add changes

--- src/circular_queue.py
+++ src/circular_queue.py
@@ -1,31 +1,30 @@
 class CircularQueue:
-    def __init__(self, max_length=5):
+    def __init__(self, max_len=5):
         self.queue = {}
         self.front = 0
         self.back = 0
-        self.max_length = max_length
+        self.max_len = max_len
 
     def remove_old_ele(self, element):
-        if (self.back + 1) % self.max_length == self.front:
-            # Queue is full, remove the oldest element
-            oldest_element = self.queue[self.front]
-            print(f"Queue is full. Removing the oldest element: {oldest_element}")
-            self.front = (self.front + 1) % self.max_length
+        if (self.back + 1) % self.max_len == self.front:
+            old_ele = self.queue[self.front]
+            print(f"removing the old ele: {old_ele}")
+            self.front = (self.front + 1) % self.max_len
 
         self.queue[self.back] = element
-        self.back = (self.back + 1) % self.max_length
-        print(f"Enqueued: {element}")
+        self.back = (self.back + 1) % self.max_len
+        print(element)
 
     def display(self):
         if self.front == self.back:
-            print("Queue is empty.")
+            print("queue is empty.")
         else:
             current = self.front
             while current != self.back:
                 print(self.queue[current], end=" ")
-                current = (current + 1) % self.max_length
+                current = (current + 1) % self.max_len
             print()
-cq1 = CircularQueue(max_length=5)
+cq1 = CircularQueue(max_len=5)
 
 cq1.remove_old_ele(1)
 cq1.remove_old_ele(2)
@@ -33,7 +32,6 @@ cq1.remove_old_ele(3)
 cq1.remove_old_ele(4)
 cq1.remove_old_ele(5)
 
-# Queue is full, removing the oldest element (1)
 cq1.remove_old_ele(6)
 
 cq1.display()
